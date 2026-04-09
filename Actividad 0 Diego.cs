037b9ca0f19c78bc12403f0e9720bde0b53568d5
Diego Rodriguez <di3gor724@gmail.com>
2026-04-09 15:59:22 -0300

    Ejercicio 0 - Validaciòn con TryParse

---
+++ Actividad 0 Diego.cs
@@ -0,0 +1,24 @@
+﻿
+
+int personas = 0;
+int bebidad = 0;
+string input;
+bool valido;
+
+valido  = false;
+while (valido == false)
+{
+    Console.WriteLine(" Ingrese la cantidad de personas que habra en esta fiesta");
+    input = Console.ReadLine();
+    if (int.TryParse(input, out personas))
+    {
+        if (personas >= 0)
+            valido = true
+                }
+    else
+    { Console.WriteLine("Error ingrese un valor entero");
+    }
+}else
+{
+    Console.WriteLine("Error ingrese un valor entero");
+}
